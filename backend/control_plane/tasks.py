5a018df978ac4366e66e5dc972d8f60363fd2f41
Elgiwawee <uz3443442@gmail.com>
2026-05-27 05:54:49 +0600

    Fix Celery task signatures

--- backend/control_plane/tasks.py
+++ backend/control_plane/tasks.py
@@ -1,5 +1,4 @@
 from celery import shared_task
-from django.db import transaction
 
 from accounts.models import Organization
 from cloud.models import CloudAccount
@@ -8,8 +7,8 @@ from ai_engine.autopilot_engine import AutopilotEngine
 from cloud.collectors.aws_ec2 import collect_ec2_instances
 
 
-@shared_task(bind=True)
-def run_autopilot_for_org(self, organization_id):
+@shared_task
+def run_autopilot_for_org(organization_id):
     """
     Trigger the full autopilot engine for an organization.
     """
@@ -21,8 +20,8 @@ def run_autopilot_for_org(self, organization_id):
     engine.run_for_organization(org)
 
 
-@shared_task(bind=True)
-def discover_cloud_account(self, cloud_account_id):
+@shared_task
+def discover_cloud_account(cloud_account_id):
     """
     Run infrastructure discovery when a cloud account is connected.
     """
@@ -31,3 +30,14 @@ def discover_cloud_account(self, cloud_account_id):
 
     if account.provider.code == "aws":
         collect_ec2_instances(account.id)
+
+
+@shared_task
+def collect_aws_ec2_task(account_id):
+    """
+    Collect AWS EC2 infrastructure.
+    """
+
+    account = CloudAccount.objects.get(id=account_id)
+
+    collect_ec2_instances(account.id)
